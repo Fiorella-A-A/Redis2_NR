33b4fe5d00dbb8990e0417f4767cbb3209e569d2
Fiorella-A-A <alexandraaquino@hotmail.com>
2023-11-29 13:18:38 -0500

    lab

---
+++ react/src/App.jsx
@@ -0,0 +1,368 @@
+import React, { useState, useEffect } from 'react';
+import CSVReader from 'react-csv-reader';
+import "./App.css"
+import * as Data from "./Data";
+
+
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, XAxis, YAxis, Legend, Tooltip } from 'recharts';
+
+
+function App() {
+
+  const [datos, setDatos] = useState(Data.default);
+
+  const [rat, setRat] = useState([])
+
+  const [pelicula, setPelicula] = useState([])
+
+  const [usuario, setUsuario] = useState(0)
+
+  useEffect(() => {
+    const obtenerdata = async () => {
+      try {
+
+        await Data.Ratings(setRat)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    obtenerdata()
+
+  }, []);
+
+  //console.log(rat)
+  console.log(pelicula)
+
+
+  const handleInputChange = (event) => {
+    setUsuario(event.target.value);
+  };
+
+
+
+
+  const handleCSVUpload = (data) => {
+    // El parámetro 'data' contendrá los datos del archivo CSV en formato de matriz
+    console.log('Datos cargados desde CSV:', data);
+    //console.log('Primeros 10 datos cargados desde CSV:', data.slice(0, 10));
+    //const yu = data.slice(0, 10)
+    setDatos(data);
+  };
+
+  const registrarCsv = async () => {
+    try {
+      await Data.postCsv(datos);
+      console.log("Datos registrados correctamente");
+    } catch (error) {
+      console.error("Error al registrar datos:", error);
+    }
+  };
+
+  const registrarDatos = async () => {
+    try {
+      const inicio = new Date();
+
+      await Data.postRiesgos( usuario);
+      /*await Data.postRiesgosdos(usuario);
+      await Data.postRiesgostres( usuario);
+      await Data.postRiesgoscuatro( usuario);
+      await Data.postRiesgoscinco(usuario);*/
+
+      const fin = new Date();
+      const tiempoDeEjecucion = fin - inicio;
+
+      console.log(`Datos registrados correctamente de api1. Tiempo de ejecución: ${tiempoDeEjecucion} milisegundos`);
+    } catch (error) {
+      console.error("Error al registrar datos:", error);
+    }
+  };
+
+
+  const registrarDatosapi2 = async () => {
+    try {
+      //await Data.postRiesgos(/*datos ,*/ usuario);
+      await Data.postRiesgosdos(/*datos ,*/ usuario);
+      console.log("Datos registrados correctamente");
+      // Inicia el temporizador
+      const inicioTiempo = performance.now();
+
+      // Tu código aquí (por ejemplo, un bucle o alguna operación intensiva)
+      for (let i = 0; i < 1000000; i++) {
+        // Operación ficticia para simular trabajo
+        //const resultado = Math.sqrt(i);
+      }
+
+      // Detén el temporizador
+      const finTiempo = performance.now();
+
+      // Calcula la diferencia de tiempo
+      const tiempoTranscurrido = finTiempo - inicioTiempo;
+
+      // Muestra el tiempo en la consola
+      console.log(`El código tardó ${tiempoTranscurrido} milisegundos.`);
+      //await Data.getListarCoseno()
+      //console.log("registro correcto")
+    } catch (error) {
+      console.error("Error al registrar datos:", error);
+    }
+  };
+  const registrarDatosapi3 = async () => {
+    try {
+      //await Data.postRiesgos(/*datos ,*/ usuario);
+      await Data.postRiesgostres(/*datos ,*/ usuario);
+      // Inicia el temporizador
+      const inicioTiempo = performance.now();
+
+      // Tu código aquí (por ejemplo, un bucle o alguna operación intensiva)
+      for (let i = 0; i < 1000000; i++) {
+        // Operación ficticia para simular trabajo
+        //const resultado = Math.sqrt(i);
+      }
+
+      // Detén el temporizador
+      const finTiempo = performance.now();
+
+      // Calcula la diferencia de tiempo
+      const tiempoTranscurrido = finTiempo - inicioTiempo;
+
+      // Muestra el tiempo en la consola
+      console.log(`El código tardó ${tiempoTranscurrido} milisegundos.`);
+      console.log("Datos registrados correctamente");
+      //await Data.getListarCoseno()
+      //console.log("registro correcto")
+    } catch (error) {
+      console.error("Error al registrar datos:", error);
+    }
+  };
+
+  const registrarDatosapi4 = async () => {
+    try {
+      //await Data.postRiesgos(/*datos ,*/ usuario);
+      await Data.postRiesgoscuatro(/*datos ,*/ usuario);
+      // Inicia el temporizador
+      const inicioTiempo = performance.now();
+
+      // Tu código aquí (por ejemplo, un bucle o alguna operación intensiva)
+      for (let i = 0; i < 1000000; i++) {
+        // Operación ficticia para simular trabajo
+        //const resultado = Math.sqrt(i);
+      }
+
+      // Detén el temporizador
+      const finTiempo = performance.now();
+
+      // Calcula la diferencia de tiempo
+      const tiempoTranscurrido = finTiempo - inicioTiempo;
+
+      // Muestra el tiempo en la consola
+      console.log(`El código tardó ${tiempoTranscurrido} milisegundos.`);
+      console.log("Datos registrados correctamente");
+      //await Data.getListarCoseno()
+      //console.log("registro correcto")
+    } catch (error) {
+      console.error("Error al registrar datos:", error);
+    }
+  };
+
+  const registrarDatosapi5 = async () => {
+    try {
+      //await Data.postRiesgos(/*datos ,*/ usuario);
+      await Data.postRiesgoscinco(/*datos ,*/ usuario);
+      // Inicia el temporizador
+      const inicioTiempo = performance.now();
+
+      // Tu código aquí (por ejemplo, un bucle o alguna operación intensiva)
+      for (let i = 0; i < 1000000; i++) {
+        // Operación ficticia para simular trabajo
+        //const resultado = Math.sqrt(i);
+      }
+
+      // Detén el temporizador
+      const finTiempo = performance.now();
+
+      // Calcula la diferencia de tiempo
+      const tiempoTranscurrido = finTiempo - inicioTiempo;
+
+      // Muestra el tiempo en la consola
+      console.log(`El código tardó ${tiempoTranscurrido} milisegundos.`);
+      console.log("Datos registrados correctamente");
+      //await Data.getListarCoseno()
+      //console.log("registro correcto")
+    } catch (error) {
+      console.error("Error al registrar datos:", error);
+    }
+  };
+
+
+  const registrarDatosx = async () => {
+    try {
+
+      await Data.getListarCoseno()
+      await Data.Ratings(setRat)
+      console.log("registro correcto")
+    } catch (error) {
+      console.error("Error al registrar datos:", error);
+    }
+  };
+
+
+
+
+  const valoresCoseno = rat.map(item => item.distancia);
+  const sumaValores = valoresCoseno.reduce((total, valor) => total + valor, 0);
+
+  console.log('Suma de todos los valores:', sumaValores);
+
+
+
+
+  const porc = ({ payload, x, y, width, height, value }) => {
+    const xy = (value.toFixed(1) * 100) / sumaValores; // Cambiado a toFixed(1)
+    const percentage = `${xy.toFixed(1)}%`; // Ajustado a un solo decimal
+    return (
+      <text x={x + width / 2} y={y} fill="#666" textAnchor="middle" dy={-6}>
+        {percentage}
+      </text>
+    );
+  };
+
+  const Recomend = async () => {
+    try {
+      await Data.getPeliculas(setPelicula)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+
+  return (
+    <>
+      <div>
+
+        <div className='cargar'>
+          <h1>hola</h1>
+          <div>
+            <img src="https://logowik.com/content/uploads/images/csv-file-format8052.jpg" alt="CSV File Format" width={50} />
+          </div>
+          <CSVReader
+            onFileLoaded={handleCSVUpload}
+            parserOptions={{ header: true, skipEmptyLines: true }}
+          />
+        </div>
+        <div>
+          <button onClick={registrarCsv}>Cargar csv</button>
+        </div>
+
+
+        <div className='btns'>
+          <button onClick={registrarDatos}>Analizar datos de csv api1</button>
+          <button onClick={registrarDatosapi2}>Analizar datos de csv api2</button>
+          <button onClick={registrarDatosapi3}>Analizar datos de csv api3</button>
+          <button onClick={registrarDatosapi4}>Analizar datos de csv api4</button>
+          <button onClick={registrarDatosapi5}>Analizar datos de csv api5</button>
+          <button onClick={() => { registrarDatos(); registrarDatosapi2(); registrarDatosapi3(); registrarDatosapi4(); registrarDatosapi5();}}>Analizar datos de csv api1,api2,api3, api4 , api 5</button>
+          <button onClick={registrarDatosx}>Generar resultados</button>
+          <div>
+            <label htmlFor="usuarioInput">Usuario:</label>
+            <input
+              type="text"
+              id="usuarioInput"
+              value={usuario}
+              onChange={handleInputChange}
+            />
+            <p>El valor del usuario es: {usuario}</p>
+          </div>
+        </div>
+
+        <div className='valores'>
+          <div className='table-vecinos'>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Id Vecino</th>
+                  <th>Distancia</th>
+                </tr>
+              </thead>
+              <tbody>
+                {rat.map(item => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.id_vecino}</td>
+                    <td>{item.distancia}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          <div className='cont-recomend' >
+            <div className='title'>
+              Peliculas recomendadas
+              <button onClick={Recomend}  >Ver</button>
+            </div>
+            <div className='box-peli'>
+              <div className='imagenes'>
+                {
+                  pelicula.map(pe => (
+                    <div className='netflix'>
+                      <img src={pe.imagen} alt="" />
+                      <p>Pelicula:{pe.id_movie}</p>
+                    </div>
+
+                  ))
+                }
+
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+
+
+
+        {/*<div className='grafico'>
+          <ResponsiveContainer width="80%" aspect={2} >
+            <BarChart
+              data={rat}
+              width={500}
+              height={300}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5
+              }}
+            >
+              <CartesianGrid strokeDasharray="4 1 2" />
+              <XAxis dataKey="id_vecino" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="distancia" fill="orange"
+
+                label={porc}
+
+              >
+
+
+              </Bar>
+
+
+
+            </BarChart>
+          </ResponsiveContainer>
+            </div>*/}
+
+
+
+      </div>
+    </>
+  );
+}
+
+export default App;
